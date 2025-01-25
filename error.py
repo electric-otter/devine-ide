e878fce1fcfec4ed265d6822afe1622db315004c
electric-otter <electric-otter-github@proton.me>
2025-01-24 22:48:57 -0500

    Update error.py

--- error.py
+++ error.py
@@ -1,2 +1,17 @@
-print("Sorry, Devine IDE encountered an issue!")
-exit()
+import os
+import tkinter as tk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+error_message = "Sorry, Devine IDE encountered an issue!"
+error_image = Image.open("error.svg")
+error_image = error_image.convert("RGBA")
+error_image_tk = ImageTk.PhotoImage(error_image)
+
+error_window = tk.Toplevel()
+error_window.title("Error")
+
+tk.Label(error_window, image=error_image_tk).pack()
+tk.Label(error_window, text=error_message).pack()
+tk.Button(error_window, text="Press any key to exit...", command=exit_program).pack()
+
+root.wait_window(error_window)
